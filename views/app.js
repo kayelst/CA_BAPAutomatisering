370ec72a9dc71e61eee852c4c48209ba98aba822
LameirBryan <lameir-bryan@hotmail.com>
2016-10-19 14:39:36 +0200

    Eddited app.js and Page.html

--- views/app.js
+++ views/app.js
@@ -1,3 +1,23 @@
+function filterInfo(){
+	var rawfile = "x";
+	var getNaam = rawfile.substring(rawfile.indexOf("tagnaam:") + 9, 
+		rawfile.indexOf("naamtag") - 1);
+	var getGitnaam = rawfile.substring(rawfile.indexOf("taggitnaam") + 12, 
+		rawfile.indexOf("gitnaamtag") - 1);
+	var getReponaam = rawfile.substring(rawfile.indexOf("tagreponaam") + 13, 
+		rawfile.indexOf("reponaamtag") - 1);
+	var getPromotor = rawfile.substring(rawfile.indexOf("tagpromotor") + 13, 
+		rawfile.indexOf("promotortag") - 1);
+	var getPhone = rawfile.substring(rawfile.indexOf("tagphone") + 10, 
+		rawfile.indexOf("phonetag") - 1);
+	var getAddress = rawfile.substring(rawfile.indexOf("tagaddress") + 12, 
+		rawfile.indexOf("addresstag") - 1);
+
+}
+
+
+
+
 var app = angular.module("myapp",[]);
 app.controller("myCtrl",function($http, $scope){
 
@@ -11,3 +31,5 @@ app.controller("myCtrl",function($http, $scope){
 
 
 });
+
+

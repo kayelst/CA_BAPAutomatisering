36600e5451e80ad20f8169143871dcb297d48c6b
kayelst <kayelst@gmail.com>
2016-10-19 14:08:42 +0200

    Maps Cleaned up

--- ClientSide/TestFiles/Info.md
+++
@@ -1,9 +0,0 @@
-# CA_BAPAutomatisering
-Een repo waar we onze files kunnen storen en samen aan eenzelfde project kunnen werken. Het project is de BAPAutomatisering voor Cloud Applications.
-
-tagnaam: freddy naamtag
-taggitnaam: lolz gitnaamtag
-tagreponaam: lolzrepo reponaamtag
-tagpromotor: bryan promotortag
-tagphone: 035584953 phonetag
-tagaddress: blablabla 25 2940 stabroek addresstag
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET Hello World page. */
-router.get('/home', function(req, res) {
   res.render('home', { title: 'Home' });
 });
 
-/* GET Userlist page. */
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
     var db = req.db;

492a76df948ae7f9e9fcee84d8f029a9a9bbf41f
kayelst <kayelst@gmail.com>
2016-12-06 13:14:15 +0100

    CommitShowing works again

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,7 +8,7 @@ var Autho = "?client_id=651b11583f0162b4cc91&client_secret=e42b41de694254d711122
 var client_id = "?client_id=651b11583f0162b4cc91"
 var UserCode = window.location.search;
 
-angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, $scope, $templateCache){
+angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, $scope, $templateCache) {
 	var apiCallRepoInfo = "https://api.github.com/repos/MyOrg1617/BAP1617_";
 	var apiCallScriptie = "https://api.github.com/repos/MyOrg1617/BAP1617_";
 	var apiCallAllStudents = "https://api.github.com/orgs/MyOrg1617/repos";
@@ -17,9 +17,9 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 	var apiCallLogCommits = "https://api.github.com/repos/MyOrg1617/BAP1617_";
 
 	//BryanCalls
-	var apiLogin = "https://github.com/login/oauth/authorize"
-	var UserToken = "https://github.com/login/oauth/access_token"
-	
+	var apiLogin = "https://github.com/login/oauth/authorize";
+	var UserToken = "https://github.com/login/oauth/access_token";
+
 	//KayCalls
 
 	var apiCallCommits = "https://api.github.com/repos/kayelst/CA_BAPAutomatisering/commits";
@@ -29,48 +29,48 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 	console.log(UserCode);
 	UserCode = UserCode.replace('?code=', '&');
 	console.log(UserCode);
-	$http.post(UserToken + Autho, {"body":UserCode});
-	
-	
+	$http.post(UserToken + Autho, {"body": UserCode});
+
+
 	//$http.post(UserToken + Autho + UserCode);
-	
+
 	//window.location.replace(UserToken + Autho + UserCode);
 
 	//var apiCallInfo2 = "/contents/README.md";
 
 	/*$http({method: 'GET', url: '/UserInfo'}).
-	success(function(data, status, headers, config) {
-		console.log('todos: ', data );
-	}).
-	error(function(data, status, headers, config) {
-		console.log('Oops and error', data);
-	});*/
+	 success(function(data, status, headers, config) {
+	 console.log('todos: ', data );
+	 }).
+	 error(function(data, status, headers, config) {
+	 console.log('Oops and error', data);
+	 });*/
 
-	$http.get( "/UserInfo").success(function( data ) {
-		$scope.A= data; //from your sample;
-		console.log( "Load was performed. " + data );
+	$http.get("/UserInfo").success(function (data) {
+		$scope.A = data; //from your sample;
+		console.log("Load was performed. " + data);
 	});
 
 	$scope.div_value = 1;
 	$scope.btnstate_repostats = true;
 	$scope.btnstate_Repohulp = false;
 	$scope.btnstate_Scriptie = false;
-	
-	$scope.Btn_RepoStats = function(){
+
+	$scope.Btn_RepoStats = function () {
 		$scope.btnstate_repostats = true;
 		$scope.btnstate_Repohulp = false;
 		$scope.btnstate_Scriptie = false;
 		$scope.div_value = 1;
 	};
 
-	$scope.Btn_RepoHulp = function(){
+	$scope.Btn_RepoHulp = function () {
 		$scope.btnstate_repostats = false;
 		$scope.btnstate_Repohulp = true;
 		$scope.btnstate_Scriptie = false;
 		$scope.div_value = 2;
 	};
 
-	$scope.Btn_Scriptie = function(){
+	$scope.Btn_Scriptie = function () {
 		$scope.btnstate_repostats = false;
 		$scope.btnstate_Repohulp = false;
 		$scope.btnstate_Scriptie = true;
@@ -81,12 +81,12 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 
 	$scope.RepoNames = [];
 
-	$scope.apiAllStudentsCall = function() {
+	$scope.apiAllStudentsCall = function () {
 		console.log(window.location.search);
 		console.log(UserCode);
 
 		console.log("apiCallAllStudents");
-		$http.get(apiCallAllStudents + Autho).then(function (response){
+		$http.get(apiCallAllStudents + Autho).then(function (response) {
 			console.log(response);
 			for (i = 0; i < response.data.length; i++) {
 				console.log("Looping - " + [i]);
@@ -96,13 +96,14 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 					RepoName = RepoName.substring(8);
 					$scope.RepoNames.push(RepoName);
 				}
-			};
-			
+			}
+			;
+
 			console.log($scope.RepoNames);
 		});
 	};
 
-	$scope.do = function(x){
+	$scope.do = function (x) {
 		$http.get(apiCallInfo + x + apiCallInfo2 + Autho).then(function (response) {
 			rawfileLink = response.data.download_url;
 			$http.get(rawfileLink).then(function (response) {
@@ -113,7 +114,7 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 
 		});
 
-		$http.get(apiCallInfo + x + Autho).then(function (response){
+		$http.get(apiCallInfo + x + Autho).then(function (response) {
 			RepoLink = response.data.html_url;
 			$scope.getRepoLink = RepoLink;
 		});
@@ -141,11 +142,11 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 			$scope.getAddress = getAddress;
 		};
 
-		$scope.RepoInfo = function(){
+		$scope.RepoInfo = function () {
 			$http.get(apiCallRepoInfo + x + "/stats/participation" + Autho).then(function (response) {
 				TotalCommit = 0;
 				console.log(response);
-				for( i = 0; i < response.data.all.length ; i++) {
+				for (i = 0; i < response.data.all.length; i++) {
 					LastCommit = response.data.all[i];
 					TotalCommit += LastCommit;
 				}
@@ -155,17 +156,17 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 
 			var currentdate = new Date();
 			var datetime = currentdate.getFullYear() + "-"
-				+ (currentdate.getMonth()+1)  + "-"
+				+ (currentdate.getMonth() + 1) + "-"
 				+ currentdate.getDate() + "T"
 				+ currentdate.getHours() + ":"
 				+ currentdate.getMinutes() + ":"
 				+ currentdate.getSeconds() + "Z";
 			console.log(datetime);
 
-			$http.get(apiCallLogCommits + x +"/commits" + Autho +"&path=Logfiles&until=" + datetime).then(function(response){
+			$http.get(apiCallLogCommits + x + "/commits" + Autho + "&path=Logfiles&until=" + datetime).then(function (response) {
 				var CurrentAmount = response.data.length;
 				console.log(CurrentAmount);
-				if (CurrentAmount > PreviousAmount){
+				if (CurrentAmount > PreviousAmount) {
 					console.log("Logs have been added");
 					PreviousAmount = CurrentAmount;
 				}
@@ -174,51 +175,45 @@ angular.module("theapp",['myapp','myapp2']).controller("myCtrl",function($http, 
 			$scope.GetCommits = function () {
 				CommitMessages = [];
 				shaArray = [];
-				$http.get(apiCallCommits + Autho).then(function(response){
-					for( i = 0; i < response.data.length ; i++) {
-
+				$http.get(apiCallCommits + Autho).then(function (response) {
+					for (i = 0; i < response.data.length; i++) {
 						CommitMessages.push(response.data[i].commit.message);
 						$scope.AllCommits = CommitMessages;
 						//shaArray.push(response.data[i].sha);
 					}
-					/*	StudentCommits = CommitArray;
-					console.log(CommitArray);
-					console.log(shaArray);
-					console.log(CommitArray[2]);*/
-					console.log(AllCommits);
+					console.log($scope.AllCommits);
 
 				});
-			}
+			};
 
-			$scope.SelectCommit = function(){
-				console.log(CommitMessages[i]);
-			}
+				$scope.idCommit = null;
+				$scope.SelectCommit = function (idCommit) {
+					$scope.idCommit = idCommit;
+					console.log(idCommit);
+				};
+
+			$scope.GetScriptie = function () {
+				$http.get(apiCallScriptie + x + "/contents/scriptie/Scriptie.md" + Autho).then(function (response) {
+					rawScriptieLink = response.data.download_url;
+					$http.get(rawScriptieLink).then(function (response) {
+						console.log(response.data);
+						ScriptieRaw = response.data;
+
+						Converter = new showdown.Converter();
+						ScriptieHtml = Converter.makeHtml(ScriptieRaw);
+						$scope.ScriptieData = $sce.trustAsHtml(ScriptieHtml);
+					});
+				});
+			};
 
 		};
 
-		$scope.GetScriptie = function() {
-			$http.get(apiCallScriptie + x + "/contents/scriptie/Scriptie.md" + Autho ).then(function (response) {
-				rawScriptieLink = response.data.download_url;
-				$http.get(rawScriptieLink).then(function (response) {
-					console.log(response.data);
-					ScriptieRaw = response.data;
+		//Login
+		$scope.SignIn = function () {
 
-					Converter = new showdown.Converter();
-					ScriptieHtml = Converter.makeHtml(ScriptieRaw);
-					$scope.ScriptieData = $sce.trustAsHtml(ScriptieHtml);
-				});
-			});
+			window.location.replace(apiLogin + client_id);
+
 		};
 
 	};
-
-	//Login
-	$scope.SignIn = function() {
-
-		window.location.replace(apiLogin + client_id);
-
-	};
-
-
 });
-

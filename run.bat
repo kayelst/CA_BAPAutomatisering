fc6c37da97cd9dec1aa02113b8c34fc5a4c2cfef
kayelst <kayelst@gmail.com>
2017-01-10 01:12:54 +0100

    Made BatchFile for easy startup

---
+++ run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+echo Moving to The Right Directory!
+cmd.exe /K "cd C:\Users\Gebruiker\Desktop\git\CA_BAPAutomatisering & echo Installing modules & npm install & echo Checking for Updates & npm update & echo Starting up Server & npm start"
